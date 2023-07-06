be2cf7b22f16766f7564326c247d9b27e3a7ea3e
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-07-06 13:54:11 +0700

    implement stack router scope for testing purpose

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,16 +1,26 @@
 import 'package:app_boilerplate/l10n/l10n.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'mock_stack_router.dart';
+
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
+    final mockStackRouter = MockStackRouter();
     return pumpWidget(
       ProviderScope(
         child: MaterialApp(
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           supportedLocales: AppLocalizations.supportedLocales,
-          home: widget,
+          home: StackRouterScope(
+            controller: mockStackRouter,
+            stateHash: 0,
+            child: Scaffold(
+              body: widget,
+            ),
+          ),
         ),
       ),
     );
